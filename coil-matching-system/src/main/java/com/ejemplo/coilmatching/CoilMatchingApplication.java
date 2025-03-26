cb3be6b4c54f4a544abfdbfff99bbb5b63d16d06
Roberto Andrade <randradep@usfq.edu.ec>
2025-03-26 16:41:38 -0500

    Add files via upload

---
+++ coil-matching-system/src/main/java/com/ejemplo/coilmatching/CoilMatchingApplication.java
@@ -0,0 +1,11 @@
+package com.ejemplo.coilmatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoilMatchingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoilMatchingApplication.class, args);
+    }
+}
